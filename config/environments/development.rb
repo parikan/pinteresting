e821f988b644bb334ba1de91f6aa6115f715ffd6
Alexandre Balay <alex77r@gmail.com>
2015-09-14 09:55:49 -0400

    Implemented Devise for Users

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,5 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -26,6 +25,9 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Required for Heroku
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
